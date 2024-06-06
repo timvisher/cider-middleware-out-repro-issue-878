3fbeb78905fa012723c92015d8541e9467be62a3
Tim Visher <tim.visher@gmail.com>
2024-06-06 09:55:07 -0400

    Revert "Add observations of post-reconnection log capture if logs are created after reconnection"
    This reverts commit 46ac68973fcedccefeaf45f3e8827a230ba57ed6.

--- cider-middleware-out-repro-issue-878.clj
+++ cider-middleware-out-repro-issue-878.clj
@@ -1,22 +1,12 @@
 (ns cider-middleware-out-repro-issue-878
   (:require [clojure.tools.logging :as log]))
 
-(defn make-log-forever-future
-  [& _]
-  (future
-    (while true
+(defn log-forever
+  []
+  (while true
     (log/info "Logging forever")
-    (Thread/sleep 1000))))
-
-(defonce log-forever-future (make-log-forever-future))
+    (Thread/sleep 1000)))
 
 (comment
-  (log/info
-   "I always show up but I don't cause previous connection's `log-forever-future`s to show back up")
-
-  (if (future-cancelled? log-forever-future)
-    (alter-var-root #'log-forever-future make-log-forever-future)
-    (log/info "Future not cancelled. Not altering"))
-
-  (future-cancel log-forever-future)
+  (log-forever)
   )
